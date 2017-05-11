18b080c9528a9b6cceaa1d80cb81f3316cb9b34d
Vaztonyo <tony.dewet7@gmail.com>
2017-05-11 14:56:35 +0200

    first commit

---
+++ greet.js
@@ -0,0 +1,17 @@
+// const assert = require('assert');
+
+// var greet = function(name){
+  //console.log('Hello ' + name);
+  // return "Hello, " + name;
+
+// };
+
+// var msg = greet('Xola');
+// console.log(msg);
+
+// assert.equal(greet('Dante'), 'Hello Dante');
+// greet('Xola')
+
+module.exports = function(name){
+    return 'Hello, ' + name;
+}
