a0fa157c70846d25c7cd8e51f6747c66aeff37c0
Vaztonyo <tony.dewet7@gmail.com>
2017-05-11 15:54:59 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,3 +1,27 @@
+const chalk = require('chalk');
+
 const greet = require('./greet');
 
 console.log(greet('Xola'))
+
+
+//import the greet module that is in the current folder
+// const greet = require('./greet');
+
+const styledMessage = chalk.bgBlue.black(greet('Xola'));
+console.log(styledMessage)
+
+var figlet = require('figlet');
+
+figlet('Hello Xola!!', {
+  font: 'Ghost',
+  horizontalLayout: 'default',
+  verticalLayout: 'default'
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
